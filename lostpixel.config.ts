bb0defb0da2c60cbd3adf22169304ba0c5d56b32
d-ivashchuk <dimaiv@me.com>
2024-03-26 12:55:25 +0100

    use correct lost pixel config

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -2,8 +2,10 @@ import { CustomProjectConfig } from "lost-pixel";
 
 export const config: CustomProjectConfig = {
   pageShots: {
-    pages: [{ path: "/", name: "landing" }],
+    pages: [],
+    pagesJsonUrl: "./lost-pixel-pages.json",
     baseUrl: "http://172.17.0.1:3000",
+    breakpoints: [640, 1024],
   },
   lostPixelProjectId: "clu8akz1i7hxri60exxc8mmoc",
   apiKey: process.env.LOST_PIXEL_API_KEY,
